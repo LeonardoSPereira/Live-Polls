f5d51547affaaf0762f3aafc7b120d0564a29a20
Leonardo Pereira <leonardo_human@hotmail.com>
2024-02-08 15:13:30 -0300

    created: create new poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,26 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
 const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.post("/polls", async (request, reply) => {
+  const createPollBodySchema = z.object({
+    title: z.string()
+  })
+  
+  const { title } = createPollBodySchema.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+
+  return reply.status(201).send({ message: "Poll created successfully", poll })
+})
 
 app.listen({ port: 3333}, () => {
   console.log("🚀HTTP Server running on port 3333");
