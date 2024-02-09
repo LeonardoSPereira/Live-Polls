c105f9729f5d0ff39fd0d1a0973cffa9f745d447
Leonardo Pereira <leonardo_human@hotmail.com>
2024-02-09 10:49:06 -0300

    created: create, list polls and vote routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,20 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
-import { z } from 'zod';
+import cookie from '@fastify/cookie'
+import { createPoll } from './routes/create-polls';
+import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
 
 const app = fastify()
 
-const prisma = new PrismaClient()
-
-app.post("/polls", async (request, reply) => {
-  const createPollBodySchema = z.object({
-    title: z.string()
-  })
-  
-  const { title } = createPollBodySchema.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title
-    }
-  })
-
-  return reply.status(201).send({ message: "Poll created successfully", poll })
+app.register(cookie, {
+  secret: "polls-app-nlw",
+  hook: "onRequest"
 })
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+
 
 app.listen({ port: 3333}, () => {
   console.log("🚀HTTP Server running on port 3333");
