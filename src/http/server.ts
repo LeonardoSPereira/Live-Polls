7912970e3aa8cf54eb4ab0a34f7ed24978538f57
Leonardo Pereira <leonardo_human@hotmail.com>
2024-02-10 08:57:02 -0300

    Add: WebSocket support for real-time poll results

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,8 @@ import cookie from '@fastify/cookie'
 import { createPoll } from './routes/create-polls';
 import { getPoll } from './routes/get-poll';
 import { voteOnPoll } from './routes/vote-on-poll';
+import fastifyWebsocket from '@fastify/websocket';
+import { pollResults } from './ws/poll-results';
 
 const app = fastify()
 
@@ -11,10 +13,12 @@ app.register(cookie, {
   hook: "onRequest"
 })
 
+app.register(fastifyWebsocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
-
+app.register(pollResults)
 
 app.listen({ port: 3333}, () => {
   console.log("🚀HTTP Server running on port 3333");
